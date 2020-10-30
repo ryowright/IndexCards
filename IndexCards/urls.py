0e862b70bf16c34de9090643c47229e99926dc44
ryowright <ryow.college@gmail.com>
2020-10-30 11:59:18 -0700

    added basic frontend with fetch on API

--- IndexCards/urls.py
+++ IndexCards/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cardsui.urls'))
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('api/', include('cardsui.urls'))
 ]
 
 urlpatterns += [
