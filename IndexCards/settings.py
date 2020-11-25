f06845e3a08f99fdf14a9f9f33e7c2df97a4849e
ryowright <ryow.college@gmail.com>
2020-11-25 06:07:58 -0800

    migrated to MySQL database

--- IndexCards/settings.py
+++ IndexCards/settings.py
@@ -99,11 +99,14 @@ WSGI_APPLICATION = 'IndexCards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'indexcardsdb',
+        'USER': 'dbadmin',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
